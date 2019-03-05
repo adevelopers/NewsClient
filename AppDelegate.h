3f03138f05186d941ddbac3271598b8f25084b44
Kirill Khudyakov <hudiakov@gmail.com>
2019-03-05 22:34:38 +0300

    Moved assets to Resources folder

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsApiOrgClient
+//
+//  Created by Kirill Khudiakov on 04/03/2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
