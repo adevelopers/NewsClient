3b0a7b83b16d1af6dc9279d02f33b8fe5a7c5e96
Kirill Khudyakov <hudiakov@gmail.com>
2019-03-05 22:41:40 +0300

    Added NewsDetailViewModel

---
+++ Screens/NewsDetail/NewsDetailViewModel/NewsDetailViewModel.h
@@ -0,0 +1,20 @@
+//
+//  NewsDetailViewModel.h
+//  NewsApiOrgClient
+//
+//  Created by Kirill Khudiakov on 05/03/2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NewsItem.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewsDetailViewModel : NSObject
+
+@property (strong) NewsItem* model;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Screens/NewsDetail/NewsDetailViewModel/NewsDetailViewModel.m
@@ -0,0 +1,15 @@
+//
+//  NewsDetailViewModel.m
+//  NewsApiOrgClient
+//
+//  Created by Kirill Khudiakov on 05/03/2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+#import "NewsDetailViewModel.h"
+
+@implementation NewsDetailViewModel
+
+
+
+@end
